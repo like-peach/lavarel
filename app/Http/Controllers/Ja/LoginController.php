7c16fa21e977a3dc187f2de8e2256e009155419a
like-peach <1719736102@qq.com>
2020-07-16 14:55:04 +0800

    demo

---
+++ app/Http/Controllers/Ja/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Ja;
+
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+   
+    public function index()
+    {
+        echo "string";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//安全与简单
+Route::get('/wh/login/{id?}/{email?}/{password?}/','Ja\LoginController@index');
