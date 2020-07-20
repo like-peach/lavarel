eed53ce5974d953e9000e8cd3de1e530066a6eec
like-peach <1719736102@qq.com>
2020-07-20 14:40:54 +0800

    2020.7.20完整

--- app/Http/Controllers/Wh/LoginController.php
+++ app/Http/Controllers/Wh/LoginController.php
@@ -13,38 +13,44 @@ class LoginController extends Controller
     //登录
     public function login(Request $Request)
     {
-    	// phpinfo();die;
-    	
+        // phpinfo();die;
+        
         return view('login');
     }
 
     //首页
     public function index(Request $Request){
-
-    	//查库
-        $data_tp=DB::select('select * from tp_admin where id = ?', [1]);
-    	var_dump($data_tp);die;
-
-    	//接收数据
-    	$data=$Request->all();
-    	// var_dump($data);die;
-        //储存
-        $data_name=$data['name'];
-        $data_pwd=$data['pwd'];
-        $name=Redis::SET("name",$data_name);
-        $pwd=Redis::SET("name",$data_pwd);
-        $name=Redis::GET("name");
-        $pwd=Redis::GET("pwd");
-
-        // var_dump($name);die;
         if ($Request->isMethod('post')) {
-        	return view('index');
-		}
+
+            //查库
+            $data_tp=DB::select('select * from tp_admin where id = ?', [1]);
+            $data_tp=json_decode(json_encode($data_tp),true);
+            $data_tp=$data_tp[0];
+            // var_dump($data_tp);die;
+
+            //接收数据
+            $data=$Request->all();
+            // var_dump($data);die;
+            //储存
+            $data_name=$data['name'];
+            $data_pwd=$data['pwd'];
+            $name=Redis::SET("name",$data_name);
+            $pwd=Redis::SET("pwd",$data_pwd);
+            $name=Redis::GET("name");
+            $pwd=md5(Redis::GET("pwd"));
+
+            if ($data_tp['user_name']!==$name || $data_tp['password']!==$pwd) {
+                
+                echo '<script>alert("登录失败");window.location.href="login";</script>';
+            }
+            return view('index');
+        }
+        return redirect('wh/login');
     }
 
     //登出
     public function loginOut(){
-    	Redis::DEL("name","pwd");
-    	return redirect('wh/login');
+        Redis::DEL("name","pwd");
+        return redirect('wh/login');
     }
 }
